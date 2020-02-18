9b258d69f6582db0572fc6cc3da7316c28b8923e
Michael Stegeman <michael@stegeman.me>
2020-02-18 12:58:49 -0900

    Update to 0.12.

--- update-tarball.sh
+++ update-tarball.sh
@@ -4,21 +4,15 @@ set -e
 
 _gateway_version="$1"
 _gateway_addon_python_version="$2"
-_intent_parser_version="$3"
 
 if [ -z "${_gateway_version}" ] || [ -z "${_gateway_addon_python_version}" ]; then
     echo "Usage:"
-    echo "    $0 <gateway version> <gateway-addon-python version> [ <intent-parser version> ]"
+    echo "    $0 <gateway version> <gateway-addon-python version>"
     exit 1
-fi
-
-if [ -z "${_intent_parser_version}" ]; then
-    _intent_parser_version="52b1d7f1f9d53d83adb813bc7bfbbbed203c0627"
 fi
 
 _gateway_url="https://github.com/mozilla-iot/gateway/archive/${_gateway_version}.tar.gz"
 _gateway_addon_python_url="https://github.com/mozilla-iot/gateway-addon-python"
-_intent_parser_url="https://github.com/mozilla-iot/intent-parser/archive/${_intent_parser_version}.zip"
 
 # Download the gateway tarball
 curl -L -o "gateway.tar.gz" "${_gateway_url}"
@@ -38,13 +32,6 @@ git clone "${_gateway_addon_python_url}" webthings-gateway/gateway-addon-python
 cd webthings-gateway/gateway-addon-python
 git checkout "v${_gateway_addon_python_version}"
 cd -
-
-# Pull down the intent parser
-curl -L -o intent-parser.zip "${_intent_parser_url}"
-unzip intent-parser.zip
-mv intent-parser-*/ webthings-gateway/intent-parser
-chmod a+x webthings-gateway/intent-parser/intent-parser-server.py
-rm intent-parser.zip
 
 # Package everything up
 tar czf "webthings-gateway_${_gateway_version}.orig.tar.gz" webthings-gateway
